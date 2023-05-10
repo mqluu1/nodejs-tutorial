3521601e4cfc4216cee393e3e5a1cb033d3d1528
mqluu1 <boskies11@gmail.com>
2023-05-10 01:54:43 -0700

    adding nodejs

---
+++ intro/app.mjs
@@ -0,0 +1,4 @@
+const action = require('./index')
+
+
+action()
---
+++ intro/errors.mjs
@@ -0,0 +1,6 @@
+import { readFile } from "fs/promises";
+
+
+const result = await readFile(new URL('app.mjs', import.meta.url), 'utf-8')
+
+console.log("hello")
